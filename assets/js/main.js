b929da03335d77391db765d386ba9d0e964d82ac
Deblyn Prado <deblynprado@hotmail.com>
2016-05-16 18:51:37 -0300

    Improving loadData function

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,16 +4,79 @@ $(function() {
   searchData();
 });
 
-function loadData() {
+/**
+ * Loading effect
+ * @param  {number} time
+ * @return {null}
+ */
+ function loadPage(time) {
+  setTimeout(showContent, time);
+  return;
+}
+
+/**
+ * Remove loading and class to hide items
+ * @return {null}
+ */
+ function showContent() {
+  $('.loading').hide();
+  $('ul').removeClass('hide');
+  return;
+}
+
+/**
+ * Load data from JSON file
+ * @return {array} items
+ */
+ function loadData() {
   var path = "assets/js/data.json"
 
   $.getJSON( path, function( data ) {
     var items = [];
     $.each( data.links, function( key, element ) {
-      var items = '<li class="item fade"><div class="votes"><svg enable-background="new 0 0 32 32" height="15px" id="Layer_1" version="1.1" viewBox="0 0 32 32" width="15px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M18.221,7.206l9.585,9.585c0.879,0.879,0.879,2.317,0,3.195l-0.8,0.801c-0.877,0.878-2.316,0.878-3.194,0  l-7.315-7.315l-7.315,7.315c-0.878,0.878-2.317,0.878-3.194,0l-0.8-0.801c-0.879-0.878-0.879-2.316,0-3.195l9.587-9.585  c0.471-0.472,1.103-0.682,1.723-0.647C17.115,6.524,17.748,6.734,18.221,7.206z" class="icon-red"/></svg><span class="number">'+ element.upvotes +'</span></div><a href="http://'+ element.meta.url + '" class="site-url">'+ element.meta.url + '</a><h2 class="title">'+ element.meta.title +'</h2><a href="#" class="category category-' + element.category +'">' + element.category +'</a><div class="user-info"><img src="assets/img/photo-small.png" alt="" class="photo" /><a href="#" class="author red-links">'+ element.meta.author +'</a><span class="time">43 Minutes ago</span><div class="comments"><i class="fa fa-comment" aria-hidden="true"></i> <a href="#" class="red-links">'+ element.comments +' Comments</a></div><a href="#" class="link-edit red-links">Edit</a></div></li>';
-      $('.data-list').append(items);
+      var info = {
+        votes: element.upvotes,
+        url: element.meta.url,
+        title: element.meta.title,
+        category: element.category,
+        author: element.author,
+        comments: element.comments
+      };
+      items.push(info);
     });
-});
+    render(items);
+    return items;
+  });
+}
+
+/**
+ * Render JSON data
+ * @param  {array} item array with json data.
+ * @return {null}
+ */
+function render(item) {
+  $.each(item, function( k, e ){
+    var listItem = '\
+    <li class="item fade"> \
+      <div class="votes"><svg enable-background="new 0 0 32 32" height="15px" id="Layer_1" version="1.1" viewBox="0 0 32 32" width="15px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M18.221,7.206l9.585,9.585c0.879,0.879,0.879,2.317,0,3.195l-0.8,0.801c-0.877,0.878-2.316,0.878-3.194,0  l-7.315-7.315l-7.315,7.315c-0.878,0.878-2.317,0.878-3.194,0l-0.8-0.801c-0.879-0.878-0.879-2.316,0-3.195l9.587-9.585  c0.471-0.472,1.103-0.682,1.723-0.647C17.115,6.524,17.748,6.734,18.221,7.206z" class="icon-red"/></svg> \
+        <span class="number">'+ e.votes +'</span> \
+      </div> \
+      <a href="http://'+ e.url + '" class="site-url">'+ e.url + '</a> \
+      <h2 class="title">'+ e.title +'</h2> \
+      <a href="#" class="category category-' + e.category +'">' + e.category +'</a> \
+      <div class="user-info"><img src="assets/img/photo-small.png" alt="" class="photo" /> \
+        <a href="#" class="author red-links">'+ e.author +'</a> \
+        <span class="time">43 Minutes ago</span> \
+        <div class="comments"> \
+          <i class="fa fa-comment" aria-hidden="true"></i> \
+          <a href="#" class="red-links">'+ e.comments +' Comments</a>\
+        </div>\
+        <a href="#" class="link-edit red-links">Edit</a>\
+      </div>\
+    </li>';
+    $('.data-list').append(listItem);
+  });
+  return;
 }
 
 function searchData() {
@@ -37,24 +100,4 @@ function searchData() {
       }
     });
   });
-}
-
-/**
- * Remove loading and class to hide items
- * @return {null}
- */
- function showContent() {
-  $('.loading').hide();
-  $('ul').removeClass('hide');
-  return;
-}
-
-/**
- * Loading effect
- * @param  {number} time
- * @return {null}
- */
- function loadPage(time) {
-  setTimeout(showContent, time);
-  return;
 }
