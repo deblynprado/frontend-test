481e180ab04c5e0e7caf71c19e580055c3c906d1
Deblyn Prado <deblynprado@gmail.com>
2016-05-17 10:36:32 -0300

    Improving JS with JSHint and fixing License in Grunt

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,7 @@ $(function() {
  * @return {array} items
  */
  function loadData() {
-  var path = "assets/js/data.json"
+  var path = "assets/js/data.json";
 
   $.getJSON( path, function( data ) {
     var items = [];
@@ -56,6 +56,7 @@ $(function() {
  */
  function render(item) {
   $.each(item, function( k, e ){
+    /*jshint multistr: true */
     var listItem = '\
     <li class="item visible fade" data-pop="'+e.votes+'" data-comments="'+e.comments+'"> \
     <div class="votes"><svg enable-background="new 0 0 32 32" height="15px" id="Layer_1" version="1.1" viewBox="0 0 32 32" width="15px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M18.221,7.206l9.585,9.585c0.879,0.879,0.879,2.317,0,3.195l-0.8,0.801c-0.877,0.878-2.316,0.878-3.194,0  l-7.315-7.315l-7.315,7.315c-0.878,0.878-2.317,0.878-3.194,0l-0.8-0.801c-0.879-0.878-0.879-2.316,0-3.195l9.587-9.585  c0.471-0.472,1.103-0.682,1.723-0.647C17.115,6.524,17.748,6.734,18.221,7.206z" class="icon-red"/></svg> \
@@ -102,7 +103,7 @@ return;
         if ((numItems === 0) && (count === 0)) {
           $('main').append('<div class="load-more">Nothing found!</div>');
           count++;
-        };
+        }
       }
     });
   });
@@ -123,19 +124,19 @@ return;
       orderASCList(listType);
     } else {
       orderDESCList(listType);
-    };
+    }
   } else {
     if (listType === "comments") {
-      var el = $('.listcomments i');
+      var elem = $('.listcomments i');
       $('.listcomments i').toggleClass('fa-sort-desc');
       $('.listcomments i').toggleClass('fa-sort-asc');
-      if ($(el).hasClass('fa-sort-desc')) {
+      if ($(elem).hasClass('fa-sort-desc')) {
         orderASCList(listType);
       } else {
         orderDESCList(listType);
-      };
-    };
-  };
+      }
+    }
+  }
   return;
 }
 
