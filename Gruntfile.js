81fceb8ef99ce1f736102ca4d7e3d5da4f92cb7b
Deblyn Prado <deblynprado@gmail.com>
2016-05-17 10:41:13 -0300

    Adding task to minify JS

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,12 +9,23 @@ module.exports = function(grunt) {
 
     jshint: {
       all: ['assets/js/main.js']
+    },
+
+    uglify: {
+      my_target: {
+        files: {
+          'assets/js/main.min.js': ['assets/js/main.js']
+        }
+      }
     }
+
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Default task(s).
   grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('build', ['jshint', 'uglify']);
 
 };
